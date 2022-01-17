0c108bdf1db0ceb57e98f35afeed97725f6e2b99
Biruk-117 <onlywork117@gmail.com>
2022-01-17 18:55:32 +0100

    Node.js server added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
-    <div className="grid-container">
+    <Router>
+        <div className="grid-container">
             <header className="row">
                 <div>
                     <a href="/" className="brand"> <img src="https://img.icons8.com/ios-filled/50/000000/checkout.png"/> Easy Way </a>
@@ -16,39 +20,17 @@ function App() {
             </header>
 
             <main>
-                <div className="row center" >
-                    {
-                        data.products.map( product => ( 
-
-                        <div key={product._id} className="card">
-                            <a href={`/product/${product._id}`}>
-
-                                <img className="medium" 
-                                src={product.image} 
-                                alt={product.name} />
-                            </a>
-
-                            <div className="card-body" >
-                                <a href={`/product/${product._id}`}>
-                                    <h2>{product.name}</h2>
-                                </a>
-
-                                <div className="price">
-                                    ${product.price}
-                                </div>
-                            </div>
-                        </div>
-                        ) )
-                    }
-                    
-                    
-                </div>
+                <Routes>
+                    <Route path = "/product/:id" element = {<ProductScreen/>} ></Route>
+                    <Route exact path = "/" element = {<HomeScreen/>} ></Route>
+                </Routes>
             </main>
 
             <footer className="row center">
                 <div>All right reserved</div>
             </footer>
         </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,24 +1,33 @@
-import React from "react";
-import Rating from "./Rating";
+import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function Product(props) {
-  const { product } = props;
-  return (
-    <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
-      </a>
+function product(props) {
+    const {product} = props
+    return (
+        <div key={product._id} className="card">
+            {//<Link to ={ `/product/${product._id}` }>
+            }
 
-      <div className="card-body">
-        <a href={`/product/${product._id}`}>
-          <h2>{product.name}</h2>
-        </a>
-        <Rating
-          rating={product.rating}
-          numReviews={product.numReviews}
-        ></Rating>
-        <div className="price">${product.price}</div>
-      </div>
-    </div>
-  );
+                <a href={`/product/${product._id}`}>
+
+                    <img className="medium" 
+                    src={product.image} 
+                    alt={product.name} />
+                </a>
+
+                <div className="card-body" >
+                    <a href={`/product/${product._id}`}>
+                        <h2>{product.name}</h2>
+                    </a>
+
+                    <div className="price">
+                        ${product.price}
+                    </div>
+                </div>
+                {//</Link>
+                }
+        </div>
+    );
 }
+
+export default product
