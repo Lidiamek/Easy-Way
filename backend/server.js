9c7cfd660f10074e9326e3aa821ac18f2c4050c2
Biruk-117 <onlywork117@gmail.com>
2022-02-01 20:39:35 +0100

    Load product from server

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,11 @@ app.get('/api/products', (req,res)=>{
 
 app.get('/', (req, res)=>{
     res.send('Server is ready');
-} )
+})
 
 
-app.listen(5000, ()=>{
-    console.log('serve at http://localhost:5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`serve at http://localhost:${port}`);
 })
